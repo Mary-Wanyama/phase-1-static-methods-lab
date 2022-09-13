9e98b1afa34f96547c254fa059daba97f86e4784
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-09-13 08:49:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 class Formatter {
   //add static methods here
+  static capitalize(str){
+    return str[0].toUpperCase() + str.slice(1, str.length).toLowerCase()
+  }
+  static sanitize(){
+    return str.replace(/[^A-Za-z0-9-']+/g, "")
+  }
+  static titleize(){
+    if (str==='from', str==='the', str==='a', str==='an', str==='of', str==='at', str==='by', str==='from') {
+      str = str.split('')
+      str = str[0].toUpperCase() + str.slice(1, str.length).toLowerCase()
+      return str
+    }
+    else{
+      return str.toUpperCase()
+    }
+  }
 }
